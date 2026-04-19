9411a5880e6707f2cc5a1668651f21098e3ee08c
Abhishek Singh Chauhan <chauhanavi667@gmail.com>
2026-04-19 12:59:00 +0530

    fix:fixed the server issue

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const http = require("http");
 
-const PORT = 3000;
-
-http.createServer((req, res) => {
-  res.end("Hello from Jenkins CI/CD 🚀");
-}).listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-function add(a,b){
-        return a+b;
+function add(a, b) {
+  return a + b;
 }
-module.exports ={add};
+
+// 👇 ONLY start server if file is run directly
+if (require.main === module) {
+  http.createServer((req, res) => {
+    res.end("Hello from Jenkins CI/CD 🚀");
+  }).listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
+
+module.exports = { add };
